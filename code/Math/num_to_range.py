75807efa7bd43733f72c512b23fb0824ee274460
liming <jj7jump@gmail.com>
2021-04-05 23:39:43 +0800

    update num to range

--- code/Math/num_to_range.py
+++ code/Math/num_to_range.py
@@ -9,7 +9,7 @@
 
 
 def num_to_range(num, inMin, inMax, outMin, outMax):
-    return (float(num - inMin) / float(inMax - inMin) * (outMax - outMin))
+    return outMin + (float(num - inMin) / float(inMax - inMin) * (outMax - outMin))
 
 
 # Examples
